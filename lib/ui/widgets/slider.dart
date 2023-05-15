eaa82ec09f336fd40174cc7ce9c1ce8cb0f828ec
amin-khademi <amin.khademi2003@gamil.com>
2023-05-15 20:39:01 +0330

    pageview swap automatically

--- lib/ui/widgets/slider.dart
+++ lib/ui/widgets/slider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nike_shop_project/common/utils.dart';
@@ -6,10 +8,44 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import 'cache_image.dart';
 
-class BannerSlider extends StatelessWidget {
-  final PageController pageController = PageController();
+class BannerSlider extends StatefulWidget {
   final List<BannerEntity> banners;
   BannerSlider({super.key, required this.banners});
+
+  @override
+  State<BannerSlider> createState() => _BannerSliderState();
+}
+
+class _BannerSliderState extends State<BannerSlider> {
+  int _currentPage = 0;
+  Timer? _timer;
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 8), (Timer timer) {
+      if (_currentPage < 2) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: Duration(milliseconds: 350),
+        curve: Curves.decelerate,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +54,21 @@ class BannerSlider extends StatelessWidget {
       child: Stack(
         children: [
           PageView.builder(
-              controller: pageController,
-              itemCount: banners.length,
+              controller: _pageController,
+              itemCount: widget.banners.length,
               physics: defaultScrollPhysics,
-              itemBuilder: (context, index) => _slide(banner: banners[index])),
+              itemBuilder: (context, index) =>
+                  _slide(banner: widget.banners[index])),
           Positioned(
             bottom: 8,
             left: 0,
             right: 0,
             child: Center(
               child: SmoothPageIndicator(
-                controller: pageController,
-                count: banners.length,
+                controller: _pageController,
+                count: widget.banners.length,
                 axisDirection: Axis.horizontal,
-                effect: ExpandingDotsEffect(
+                effect: SwapEffect(
                     spacing: 4,
                     radius: 20,
                     dotWidth: 10,
