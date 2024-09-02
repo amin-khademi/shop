890ea4f68b9b39f1c76b81ff17771fcfa539a34d
amin.khademi <120728947+amin-khademi@users.noreply.github.com>
2024-09-02 23:28:28 +0330

    android build problem solved

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.getInstance();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,49 +22,17 @@ class MyApp extends StatelessWidget {
     }).catchError((onError) {
       debugPrint(onError.toString());
     });
-    const defaultTextStyle = TextStyle(fontFamily: "IranYekan");
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
       ],
-      supportedLocales: const [
-        Locale('fa'), // farsi
-      ],
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        snackBarTheme:
-            const SnackBarThemeData(contentTextStyle: defaultTextStyle),
-        textTheme: TextTheme(
-          titleMedium: defaultTextStyle.copyWith(
-            fontSize: 14,
-            color: LightThemeColor.secondaryTextColor,
-          ),
-          labelLarge:
-              defaultTextStyle.apply(color: LightThemeColor.secondaryTextColor),
-          bodyMedium: defaultTextStyle,
-          titleLarge: defaultTextStyle.copyWith(
-              fontWeight: FontWeight.bold, fontSize: 15),
-          bodySmall:
-              defaultTextStyle.apply(color: LightThemeColor.secondaryTextColor),
-        ),
-        colorScheme: const ColorScheme.light(
-            primary: LightThemeColor.primaryColor,
-            secondary: LightThemeColor.secondaryColor,
-            onSecondary: Colors.white),
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      supportedLocales: const [Locale('fa')],
+      title: 'Nike Shop',
+      theme: AppTheme.lightTheme,
       home: const RootScreen(),
     );
   }
--- lib/theme.dart
+++ lib/theme.dart
@@ -6,3 +6,28 @@ class LightThemeColor {
   static const primaryTextColor = Color(0xff262A35);
   static const secondaryTextColor = Color(0xffB3B6BE);
 }
+
+class AppTheme {
+  static const defaultTextStyle = TextStyle(fontFamily: "IranYekan");
+  static ThemeData lightTheme = ThemeData(
+    snackBarTheme: const SnackBarThemeData(contentTextStyle: defaultTextStyle),
+    textTheme: TextTheme(
+      titleMedium: defaultTextStyle.copyWith(
+        fontSize: 14,
+        color: LightThemeColor.secondaryTextColor,
+      ),
+      labelLarge:
+          defaultTextStyle.apply(color: LightThemeColor.secondaryTextColor),
+      bodyMedium: defaultTextStyle,
+      titleLarge:
+          defaultTextStyle.copyWith(fontWeight: FontWeight.bold, fontSize: 15),
+      bodySmall:
+          defaultTextStyle.apply(color: LightThemeColor.secondaryTextColor),
+    ),
+    colorScheme: const ColorScheme.light(
+        primary: LightThemeColor.primaryColor,
+        secondary: LightThemeColor.secondaryColor,
+        onSecondary: Colors.white),
+    primarySwatch: Colors.blue,
+  );
+}
